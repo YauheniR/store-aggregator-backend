3e890f49bbab0291457a38ea0cbc62a0d11004c0
Yauheni <Yauheni.conn@gmail.com>
2021-05-29 22:26:33 +0400

    refactoring - changed extend_schema_view

--- src/products/models.py
+++ src/products/models.py
@@ -16,8 +16,8 @@ class ProductModel(models.Model):
 
 class ProviderProductModel(models.Model):
     name = models.CharField(max_length=200)
-    price = models.FloatField(help_text='Price', validators=[validators.MinValueValidator(0)])
-    code = models.IntegerField(help_text='Code', validators=[validators.MinValueValidator(0)])
+    price = models.FloatField(validators=[validators.MinValueValidator(0)],)
+    code = models.IntegerField(validators=[validators.MinValueValidator(0)],)
     product = models.ForeignKey('products.ProductModel', on_delete=models.CASCADE)
     provide = models.ForeignKey('providers.ProviderModel', on_delete=models.CASCADE)
     created = models.DateField(auto_now_add=True)
